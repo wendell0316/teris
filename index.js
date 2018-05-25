a4255837cf0093441d57437f2f56c8e6f7a42bd8
王栋 <wendell.wang@eoitek.com>
2018-05-25 15:21:02 +0800

    删除deepClone()函数，用map和JSON.parse(JSON.stringfy())来代替

--- index.js
+++ index.js
@@ -2,61 +2,10 @@
 let score = 0; // 计分
 let downTimer = 800;
 const row = 10, col = 20;
-function deepClone(obj) {
-  if (obj == null || typeof obj !== 'object') {
-    return obj;
-  }
-
-  switch (Object.prototype.toString.call(obj)) {
-    case '[object Array]': {
-      const result = new Array(obj.length);
-      for (let i=0; i<result.length; ++i) {
-        result[i] = deepClone(obj[i]);
-      }
-      return result;
-    }
-
-    // Object.prototype.toString.call(new XxxError) returns '[object Error]'
-    case '[object Error]': {
-      const result = new obj.constructor(obj.message);
-      result.stack = obj.stack; // hack...
-      return result;
-    }
-
-    case '[object Date]':
-    case '[object RegExp]':
-    case '[object Int8Array]':
-    case '[object Uint8Array]':
-    case '[object Uint8ClampedArray]':
-    case '[object Int16Array]':
-    case '[object Uint16Array]':
-    case '[object Int32Array]':
-    case '[object Uint32Array]':
-    case '[object Float32Array]':
-    case '[object Float64Array]':
-    case '[object Map]':
-    case '[object Set]':
-      return new obj.constructor(obj);
-
-    case '[object Object]': {
-      const keys = Object.keys(obj);
-      const result = {};
-      for (let i=0; i<keys.length; ++i) {
-        const key = keys[i];
-        result[key] = deepClone(obj[key]);
-      }
-      return result;
-    }
-
-    default: {
-      throw new Error("Unable to copy obj! Its type isn't supported.");
-    }
-  }
-}
 
 class Tetris {
   constructor(shape) {
-    this.position = deepClone(shape);
+    this.position = JSON.parse(JSON.stringify(shape))
     this.rotateCoodinate = this.position.pop();
     for (const pos of this.position) {
       pos.x += 4;
@@ -96,11 +45,7 @@ class Tetris {
   }
 
   move(x, y) {
-    const newPos = deepClone(this.position);
-    newPos.forEach(pos => {
-      pos.x += x;
-      pos.y += y;
-    });
+    const newPos = this.position.map(pos => ({x: pos.x + x, y: pos.y + y}));
     if (Tetris.isValidPos(newPos)) {
       if (this.rotateCoodinate !== null) {
         this.rotateCoodinate.rX += x;
@@ -108,6 +53,7 @@ class Tetris {
       }
       this.position = newPos;
       this.draw();
+      return true;
     }
   }
 
@@ -129,7 +75,7 @@ class Tetris {
       rect.classList.remove('active');
       rect.classList.add('fix');
       rect.setAttribute('x-field', `${pos.x}`); // 标记每个固定方块的位置。
-      rect.setAttribute('y-field', `${pos.y}`)
+      rect.setAttribute('y-field', `${pos.y}`);
     }
   }
 
@@ -223,13 +169,8 @@ function createNextShape() {
 }
 
 function moveDown() {
-  const newPos = deepClone(shape.position);
-  newPos.forEach(pos => {
-    pos.y += 1;
-  });
-  if (Tetris.isValidPos(newPos)) {
-    shape.move(0, 1);
-  } else {
+   const moveStatus = shape.move(0, 1);
+   if (!moveStatus) {
     shape.fix();
     shape.remove();
     [...document.querySelector('.next').children].map(x => x.remove());
